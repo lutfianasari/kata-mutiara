e89f8325be49dbe8e9078bfaffdb938f38259a03
lutfianasari <lutefnyew@gmail.com>
2013-03-26 17:15:13 +0900

    correct devise db:migrate

--- db/migrate/20130320024937_devise_create_users.rb
+++ db/migrate/20130320024937_devise_create_users.rb
@@ -1,50 +1,48 @@
 class DeviseCreateUsers < ActiveRecord::Migration
+
   def self.up
     create_table(:users) do |t|
       #t.database_authenticable :null => false
       ## Database authenticatable
-      #t.string :email,              :null => false, :default => ""
-      #t.string :encrypted_password, :null => false, :default => ""
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
 
-      t.recoverable
+      #t.recoverable
       ## Recoverable
-      #t.string   :reset_password_token
-      #t.datetime :reset_password_sent_at
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
-      t.rememberable
+      #t.rememberable
       ## Rememberable
-      #t.datetime :remember_created_at
+      t.datetime :remember_created_at
 
-      t.trackable
+      #t.trackable
       ## Trackable
-      #t.integer  :sign_in_count, :default => 0
-      #t.datetime :current_sign_in_at
-      #t.datetime :last_sign_in_at
-      #t.string   :current_sign_in_ip
-      #t.string   :last_sign_in_ip
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       t.string :username
-
       #t.encryptable
-      
-      t.confirmable
-      #t.lockable :lock_strategy => failed_attemps, :unlock_strategy => both
-      #t.token_authenticable
-      ## Confirmable
-      #t.string   :confirmation_token
-      #t.datetime :confirmed_at
-      #t.datetime :confirmation_sent_at
-      #t.string   :unconfirmed_email # Only if using reconfirmable
 
+      #t.confirmable
+      ## Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
+      
       t.timestamps
     end
 
-    #add_index :users, :email,                :unique => true
+    add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
-  end
+  end  
 
   def self.down
     drop_table :users 
